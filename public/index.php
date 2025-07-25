d3c6a793a2459a645399727511b851352c15a64b
vsluke <137455060+vsluke@users.noreply.github.com>
2025-07-25 17:39:04 +1200

    исправил index.php

--- public/index.php
+++ public/index.php
@@ -1,17 +1,27 @@
 <?php
-// Получаем входящие данные от Telegram
-$update = json_decode(file_get_contents("php://input"), true);
 
-// Пример простой реакции на сообщение
-if (isset($update["message"]["text"])) {
-    $text = $update["message"]["text"];
-    $chat_id = $update["message"]["chat"]["id"];
+error_reporting(-1);
+ini_set('display_errors', 0);
+ini_set('log_errors', 'on');
+ini_set('error_log', __DIR__ . '/errors.log');
 
-    // Отправка ответа
-    file_get_contents("https://api.telegram.org/bot" . getenv("BOT_TOKEN") .
-        "/sendMessage?chat_id=$chat_id&text=" . urlencode("Вы сказали: $text"));
+require_once 'config.php';
+require_once 'functions.php';
+
+// var_dump(BASE_URL . 'setWebhook?url=https://immigrantnz.info/bots/1/');
+
+$update = json_decode(file_get_contents('php://input'));
+
+debug($update);
+
+$text = isset($update->message->text) ? "You wrote: <i>{$update->message->text}</i>" : "<u>I need some text</u>";
+$chat_id = $update->message->chat->id ?? 0;
+$name = $update->message->from->first_name ?? 'Guest';
+
+if ($chat_id) {
+    $res = send_request('sendMessage', [
+        'chat_id' => $chat_id,
+        'text' => "Hello, <b>{$name}</b>!" . PHP_EOL . $text,
+        'parse_mode' => 'HTML',
+    ]);
 }
-
-// Обязательно верни 200 OK, чтобы Telegram не ругался
-http_response_code(200);
-echo "OK";
